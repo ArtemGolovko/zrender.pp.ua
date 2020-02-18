9c3e1c35d89c62afc380be89110ddbc01f6bfed9
ArtemusG <artem.golovko2008@gmail.com>
2020-02-18 13:58:53 +0200

    learn Ajax

---
+++ Test/forms/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://code.jquery.com/jquery-2.1.0.js"></script>
+    <script src="./script.js"></script>
+</head>
+<body>
+    <form action="./form.php" method="post">
+        <input type="text" name="text">
+        <input type="password" name="pass">
+        <input type="submit" value="send">
+    </form>
+</body>
+</html>
