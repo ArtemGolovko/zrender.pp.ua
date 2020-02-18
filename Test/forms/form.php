3277b88fec0d4e2ae3995e54b77abbf737afb2f6
ArtemusG <artem.golovko2008@gmail.com>
2020-02-18 14:59:10 +0200

    ajax...

--- Test/forms/form.php
+++ Test/forms/form.php
@@ -1,5 +1,8 @@
 <?php
-
+/*if($_POST["text"] === "1234"){
+    echo "unsuccess wrong_name";
+} else {
+    echo "success";
+}*/
 var_dump($_POST);
-
 ?>
--- Test/forms/script.js
+++ Test/forms/script.js
@@ -9,7 +9,16 @@ $(document).ready(function () {
             cache: false,
             processData: false,
             success: function (result){
-                alert(result)
+                /*$("#errortext").text("");
+                var res = result.split(" ");
+                if(res[0] === "success"){
+                    alert(/res[1]/"success");
+                } else if (res[0] === "unsuccess"){
+                    if(res[1] === "wrong_name"){
+                        $("#errortext").text("Wrong name!!!");
+                    }
+                }*/
+                alert(result);
             }
         });
     });
