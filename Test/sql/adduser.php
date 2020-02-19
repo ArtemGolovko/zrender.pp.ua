876faf6de9a76b4c370105bd1d8e114d73d4f78b
ArtemusG <artem.golovko2008@gmail.com>
2020-02-19 14:38:20 +0200

    "....'".$var."'..." => ".....'$var'....."

--- Test/sql/adduser.php
+++ Test/sql/adduser.php
@@ -7,7 +7,7 @@ $pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
 
 $pass = md5($pass."gcxhcgmhmhm5551");
 
-$sql = "INSERT INTO `test-logs` (`login`, `email`, `pass`) VALUES('".$login."', '".$email."', '".$pass."')";
+$sql = "INSERT INTO `test-logs` (`login`, `email`, `pass`) VALUES('$login', '$email', '$pass')";
 $query = $pdo->prepare($sql);
 $query->execute();
 echo "OK";
