e233294f20afcc430a013cd07228ff4f90b718d9
Artem_Golovko <artem.golovko2008@gmail.com>
2020-04-19 17:05:59 +0300

    clean my test dir

--- test/etc/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require( dirname( __FILE__ ) . '/loader.php');
-require( TEKA . '/header.php');
-require( TEKA . '/body-header.php');
-?>
-    </body>
-</html>
--- test/etc/server/log.php
+++
@@ -1,5 +0,0 @@
-<?php/*
-
-$pass = $_POST['pass'];
-$email = $_POST['email'];
-echo "Hello ". $email . "! I know your password: " . $pass;
